3abb08818ad8029246669382fd3af152c1172a69
Svetlana Galygina <svetlanka.woman@gmail.com>
2022-02-27 22:13:57 +0200

    layout question__grade

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  const questionGrades = document.querySelectorAll('.question__grade');
+  const questionGrades = document.querySelectorAll('.question__grade'),
+        arrColorsGrade = ['#8D0205', '#AE0D07', '#FE5A59', '#FF8353', '#E2A913', '#FFA400', '#D7E317', '#C5F65C', '#00E355', '#00AB23', '#079D26'];
+
 
   function renderQuestionGrades() {
     questionGrades.forEach((grade) => {
@@ -11,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
         grade.innerHTML += `
         <li>
           <input type="radio" id="${questionId}-${i}" name="${questionId}" value="${i}">
-          <label for="${questionId}-${i}">${i}</label>
+          <label style="border-color: ${arrColorsGrade[i]}" class="label-${i}" for="${questionId}-${i}">${i}</label>
         </li>
         `
       }

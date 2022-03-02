da43d4dc82b71a0059a01cc527d40638aeb98da7
Svetlana Galygina <svetlanka.woman@gmail.com>
2022-03-02 21:48:31 +0200

    start-page buttom

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const questionGrades = document.querySelectorAll('.question__grade'),
         arrColorsGrade = ['#8D0205', '#AE0D07', '#FE5A59', '#FF8353', '#E2A913', '#FFA400', '#D7E317', '#C5F65C', '#00E355', '#00AB23', '#079D26'];
 
-
   function renderQuestionGrades() {
     questionGrades.forEach(grade => {
       const questionId = grade.parentElement.parentElement.getAttribute('id');
@@ -32,7 +31,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }
         input.addEventListener('change', onchangeInputGrade);
       });
-
     });
   }
 
@@ -48,19 +46,29 @@ window.addEventListener('DOMContentLoaded', () => {
       elem.style.cssText = 'height:' + elem.scrollHeight + 'px';
     });
   });
+
+  const startButton = document.getElementById('start-button'),
+        startPage = document.querySelector('.start-page'),
+        questionPage = document.querySelector('.question-page');
+
+  startButton.addEventListener('click', () => {
+    startPage.classList.toggle('hide');
+    questionPage.classList.toggle('hide');
+    questionPage.classList.toggle('fade');
+  })
   
   new Swiper('.swiper', {
     pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-          renderFraction: function(currentClass, totalClass) {
-            return `
-              <span class="question-current ${currentClass}"></span>
-              /
-              <span class="question-total ${totalClass}"></span>
-              `
-          }
-        },
+      el: '.swiper-pagination',
+      type: 'fraction',
+      renderFraction: function(currentClass, totalClass) {
+        return `
+          <span class="question-current ${currentClass}"></span>
+          /
+          <span class="question-total ${totalClass}"></span>
+          `
+      }
+    },
   });
   // const swiper = new Swiper('.swiper', {
   //   // Optional parameters
